07662cab15791cf8692bc1db67dd11cf6a03ac55
Tito <titotroffe@hotmail.com>
2026-09-11 06:43:53 -0300

    fix modulo laboratorio detalles de estilo

--- resources/views/laboratorio/index.blade.php
+++ resources/views/laboratorio/index.blade.php
@@ -241,11 +241,6 @@
                     <input type="hidden" name="_section" value="cruda">
                     
                     <div class="flex flex-col items-center mb-8">
-                        <label class="text-[10px] font-bold mb-2 tracking-wide text-slate-400 mt-2">FECHA</label>
-                        <input type="date" name="fecha" value="{{ old('fecha', date('Y-m-d')) }}" max="{{ date('Y-m-d') }}" class="w-48 bg-slate-900 border border-slate-600 rounded p-2 text-center text-white focus:outline-none focus:border-blue-500 font-mono mb-4" required>
-                    </div>
-
-                    <div class="flex flex-col items-center mb-8 border-t border-slate-700 pt-6">
                         <label class="text-[10px] font-bold mb-2 tracking-wide text-slate-400">FRECUENCIA DE ANÁLISIS</label>
                         <select name="frecuencia_cruda" class="w-64 bg-slate-900 border border-slate-600 rounded p-2 text-center focus:outline-none focus:border-blue-500 text-[10px] font-bold tracking-wide text-slate-400 uppercase mb-4" required onchange="toggleAguaCrudaFields(this.value)">
                             <option value="">Seleccionar Frecuencia</option>
@@ -253,6 +248,9 @@
                                 <option value="{{ $frecuencia->id }}" {{ old('frecuencia_cruda') == $frecuencia->id ? 'selected' : '' }}>Análisis {{ $frecuencia->nombre }}</option>
                             @endforeach
                         </select>
+
+                        <label class="text-[10px] font-bold mb-2 tracking-wide text-slate-400 mt-2">FECHA</label>
+                        <input type="date" name="fecha" value="{{ old('fecha', date('Y-m-d')) }}" max="{{ date('Y-m-d') }}" class="w-48 bg-slate-900 border border-slate-600 rounded p-2 text-center text-white focus:outline-none focus:border-blue-500 font-mono mb-4" required>
                     </div>
 
                     <div id="campos-cruda" class="w-full">
